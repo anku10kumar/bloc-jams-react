e1339613bdaaac56521e7db31b34746524f36a21
greengrass100 <anku10kumar@gmail.com>
2019-02-20 10:42:30 -0600

    new file added

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+ class PlayerBar extends Component {
+   render() {
+     return (
+       <section className="player-bar">
+         <section id="buttons">
+            <button id="previous" onClick={this.props.handlePrevClick}>
+             <ion-icon name="skip-backward"></ion-icon>
+           </button>
+           <button id="play-pause" onClick={this.props.handleSongClick} >
+             <ion-icon name={this.props.isPlaying ? 'pause' : 'play'}></ion-icon>
+
+           </button>
+           <button id="next" onClick={this.props.handleNextClick}>
+             <ion-icon name="skip-forward"></ion-icon>
+           </button>
+         </section>
+         <section id="time-control">
+           <div className="current-time">–:––</div>
+         <input type="range" className="seek-bar" defaultValue="0" />
+           <div className="total-time">–:––</div>
+         </section>
+         <section id="volume-control">
+           <div className="icon ion-volume-low"></div>
+           <input type="range" className="seek-bar" defaultValue="80" />
+           <div className="icon ion-volume-high"></div>
+         </section>
+       </section>
+     );
+   }
+ }
+
+ export default PlayerBar;
