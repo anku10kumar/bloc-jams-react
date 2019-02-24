af0213fa8e07131675b28e6758eaf72d47179295
greengrass100 <anku10kumar@gmail.com>
2019-02-24 12:54:26 -0600

    checkpoint code committed

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
 
- class PlayerBar extends Component {
-   render() {
-     return (
-       <section className="player-bar">
-         <section id="buttons">
-            <button id="previous" onClick={this.props.handlePrevClick}>
-             <ion-icon name="skip-backward"></ion-icon>
-           </button>
-           <button id="play-pause" onClick={this.props.handleSongClick} >
-             <ion-icon name={this.props.isPlaying ? 'pause' : 'play'}></ion-icon>
+class PlayerBar extends Component {
+  render() {
+    return (
+      <section className="player-bar">
+        <section id="buttons">
+          <button id="previous" onClick={this.props.handlePrevClick}>
+            <ion-icon name="skip-backward"></ion-icon>
+        </button>
+        <button id="play-pause" onClick={this.props.handleSongClick} >
+          <ion-icon name={this.props.isPlaying ? 'pause' : 'play'}></ion-icon>
 
-           </button>
-           <button id="next" onClick={this.props.handleNextClick}>
-             <ion-icon name="skip-forward"></ion-icon>
-           </button>
-         </section>
-         <section id="time-control">
-           <div className="current-time">–:––</div>
-         <input type="range" className="seek-bar" defaultValue="0" />
-           <div className="total-time">–:––</div>
-         </section>
-         <section id="volume-control">
-           <div className="icon ion-volume-low"></div>
-           <input type="range" className="seek-bar" defaultValue="80" />
-           <div className="icon ion-volume-high"></div>
-         </section>
-       </section>
-     );
-   }
- }
+      </button>
+      <button id="next" onClick={this.props.handleNextClick}>
+        <ion-icon name="skip-forward"></ion-icon>
+    </button>
+  </section>
+  <section id="time-control">
+    <div className="current-time">{this.props.currentTime}</div>
+  <input
+    type="range"
+    className="seek-bar"
+    value={(this.props.currentTime / this.props.duration) || 0}
+    max="1"
+    min="0"
+    step="0.01"
+    onChange={this.props.handleTimeChange}
+  />
+  <div className="total-time">{this.props.formatTime(this.props.currentTime)} / {this.props.formatTime(this.props.duration)}</div>
+</section>
 
- export default PlayerBar;
+<section id="volume-control">
+  <div className="current-volume">{this.props.currentVolume}</div>
+
+<input
+  type="range"
+  className="volume-bar"
+  value={this.props.currentVolume || 1}
+  min="0"
+  max="100"
+  step="0.01"
+  onChange={this.props.handleVolumeChange}
+/>
+
+<div className="total-volume">{this.props.length}</div>
+</section>
+</section>
+);
+}
+}
+
+export default PlayerBar;
